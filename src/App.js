803790bc1ff4fee48fabd5cc2511a1ecf93497bc
Akhil S <amitrath096@gmail.com>
2022-09-04 15:19:27 +0530

    changes:added cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './shared/header';
+import { RentalCard } from './components/rental/rentalcard';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Test
+      <div className='container'>
+        <section id='rentalListing'>
+          <h1 className='page-title'>Your Home All Around the World</h1>
+          <div className='row'>
+            <RentalCard />
+            <RentalCard />
+            <RentalCard />
+            <RentalCard />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }

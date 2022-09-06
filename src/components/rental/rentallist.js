25385c2bdeca2650dfb5537894df3fc2795e9a6d
Akhil S <amitrath096@gmail.com>
2022-09-06 23:11:55 +0530

    added store aand action

--- src/components/rental/rentallist.js
+++ src/components/rental/rentallist.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RentalCard } from "./rentalcard";
 import { connect } from 'react-redux';
+import * as action from '../../actions';
 
- function RentalList(props) {
+function RentalList(props) {
     function rentallist() {
         return (props.rentals.map((rental, index) => {
             return <RentalCard rentalData={rental} key={index} />
         })
         );
     }
+    useEffect(() => {
+        props.dispatch(action.fetchRentals());
+    });
     function rentalListing() {
         return (<section id='rentalListing'>
             <h1 className='page-title'>Your Home All Around the World</h1>
@@ -25,7 +29,7 @@ import { connect } from 'react-redux';
 
 function mapStateToProps(state) {
     return {
-        rentals: state.rentals
+        rentals: state.rentals.data
     }
 }
 

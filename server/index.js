046d241883cc64bdbf3b3220e2f738563eb3873a
Akhil S <amitrath096@gmail.com>
2022-09-14 00:50:25 +0530

    db changes add basic route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');;
 const app = express();
 const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+const rentalRoutes = require('./routes/rental');
 
-app.get('/', (req, res) => {
-    res.json({ success: true });
-})
+mongoose.connect(config.DB_URL).then(() => {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDB();
+});
+app.use('/api/v1/rentals', rentalRoutes);
 
 app.listen(PORT, () => {
     console.log(`node is running at port ${PORT}`)
